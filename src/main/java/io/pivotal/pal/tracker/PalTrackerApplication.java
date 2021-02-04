c0770dd12af2f5ad5a85b4ece7955d07ce8aa0bb
Mohammed Zeshan <MZUnixLinux@gmail.com>
2021-02-04 18:54:24 +0000

    git cherry-pick spring-boot-solution
    git cherry-pick configuration-start
    
    git cherry-pick configuration-solution
    
    git cherry-pick pipeline-start
    
    git cherry-pick pipeline-solution
    
    git cherry-pick mvc-start
    
    git cherry-pick mvc-solution
    
    git cherry-pick migration-start
    
    git cherry-pick migration-solution
    
    git cherry-pick jdbc-start
    
    git cherry-pick jdbc-solution

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -0,0 +1,20 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class PalTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class, args);
+    }
+
+    @Bean
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
+    }
+}
